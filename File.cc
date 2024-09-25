8732726288bd5ba6e3dd98adb7bebe46a4f9396f
Naa <naakuyea.na@gmail.com>
2024-09-24 20:50:18 -0400

    first attempt at a commit

---
+++ File.cc
@@ -0,0 +1,26 @@
+#include "File.h"
+
+File::File(string name, string content, Date& date){
+    this->name = name;
+    this->content = content;
+    this->date = date;
+}
+
+bool File::lessThan(Date& d){
+    if(this->date.lessThan(d)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+void File::print(){
+    cout << "File title: " << name << endl;
+    cout << "Date added: ";
+    date.print();
+}
+
+void File::printContents(){
+    this->print();
+    cout << "Content: " << content <<endl;
+}
